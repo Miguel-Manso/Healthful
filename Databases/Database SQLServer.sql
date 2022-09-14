144496540e5fea3881fc281076a1dcf3a1bef62b
Miguel-Manso <miguelfilipemanso13@gmail.com>
2022-09-14 07:32:11 -0300

    Update Database SQLServer.sql

--- Databases/Database SQLServer.sql
+++ Databases/Database SQLServer.sql
@@ -1,59 +1,59 @@
-create database healthfultcc;
+create database healthful;
 
-use healthfultcc;
+use healthful;
 
 CREATE TABLE usuario (
-    id_User int identity PRIMARY KEY NOT NULL,
-    nome_User varchar (40) unique NOT NULL,
-    email_User varchar (30) NOT NULL,
-    senha_User varchar (30) NOT NULL,
-    dt_nasc varchar (10),
-    nivel_User int NOT NULL,
-    status_User int,
-    data_insert varchar (10),
-    data_update varchar (10),
+    idUser int identity (1,1) PRIMARY KEY NOT NULL,
+    nomeUser varchar (40) unique NOT NULL,
+    emailUser varchar (30) NOT NULL,
+    senhaUser varchar (30) NOT NULL,
+    dtNasc varchar (10),
+    nivelUser int NOT NULL,
+    statusUser int,
+    dataInsert varchar (10),
+    dataUpdate varchar (10),
 );
 
 
 CREATE TABLE categoria (
-    id_categoria int identity NOT NULL,
-    nome_categoria varchar (20) unique NOT NULL,
-     status_categoria int,
-    data_insert varchar (10),
-    data_update varchar (10),
-    PRIMARY KEY (id_categoria)
+    idCategoria int identity (1,1) NOT NULL,
+    nomeCategoria varchar (20) unique NOT NULL,
+     statusCategoria int,
+    dataInsert varchar (10),
+    dataUpdate varchar (10),
+    PRIMARY KEY (idCategoria)
 );
 
 
 
 
 CREATE TABLE postagem (
-    id_postagem int identity NOT NULL,
-    nome_post varchar (20) NOT NULL,
-    text_post varchar (8000),
-    data_insert varchar (10),
-    data_update varchar (10),
-    status_post int ,
-    FK_id_User int,
-    FK_id_categoria int,
-    PRIMARY KEY (id_postagem),
-    Foreign key (FK_id_User) references usuario (id_User),
-    Foreign key (FK_id_categoria) references categoria (id_categoria)
+    idPost int identity (1,1) NOT NULL,
+    nomePost varchar (20) NOT NULL,
+    textPost varchar (8000),
+    dataInsert varchar (10),
+    dataUpdate varchar (10),
+    statusPost int ,
+    FK_idUser int,
+    FK_idCategoria int,
+    PRIMARY KEY (idPost),
+    Foreign key (FK_idUser) references usuario (idUser),
+    Foreign key (FK_idCategoria) references categoria (idCategoria)
 );
 
 
 
 
 CREATE TABLE comentario (
-    id_comentario int identity NOT NULL,
-    texto_coment varchar (3000),
-    status_coment int,
-    data_insert varchar (10),
-    data_update varchar (10),
-    FK_id_User int,
-    FK_id_postagem int,
-    PRIMARY KEY (id_comentario),
-    Foreign key (FK_id_User) references usuario (id_User),
-    Foreign key (FK_id_postagem) references postagem (id_postagem)
+    idComent int identity (1,1) NOT NULL,
+    textoComent varchar (3000),
+    statusComent int,
+    dataInsert varchar (10),
+    dataUpdate varchar (10),
+    FK_idUser int,
+    FK_idPost int,
+    PRIMARY KEY (idComent),
+    Foreign key (FK_idUser) references usuario (idUser),
+    Foreign key (FK_idPost) references postagem (idPost)
 );
 
