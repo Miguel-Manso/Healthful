95d9447169b810163bd5882206ddcd5eaa5ca7ad
VictorHBellini0 <victorfebellini@gmail.com>
2022-11-02 13:54:11 -0300

    Home em Andamento

--- Healthful/src/Pages/Home/home.jsx
+++ Healthful/src/Pages/Home/home.jsx
@@ -1,36 +1,55 @@
-import { Footer } from "../../Components/Footer/footer.jsx"
-import { Navbar } from "../../Components/Navbar/navbar.jsx"
-import { Users } from "../../Components/Users/users.jsx"
-import { Container, Titulo, Paragrafos, Secao, Titulos, Texto} from "./style.jsx"
+import { Footer } from "../../Components/Footer/footer.jsx";
+import { Navbar } from "../../Components/Navbar/navbar.jsx";
+import loguinha from "../../Assets/logo.svg";
+import { Container, Seção, Titulos, Titulo, Cartão, TextoCartão, TituloCartão, ImagemCartão} from "./style.jsx";
 
 
 export function Home () {
-    return (
-<>
-        <Container>
-            <Navbar />
+  return (
+    <>
+      <Container>
+        <Navbar />
             
-            <Secao>
-                <Titulos>
-                    <Titulo>“Que desgraça é para o  <br />  homem envelhecer sem <br /> nunca ver a beleza e a força <br /> do que o seu corpo é capaz”</Titulo>
-                    <span>Sócrates</span >
-                </Titulos> 
-            </Secao>
-                    <Paragrafos>
-                        <Texto>
-                        O mergulho é a atividade de submergir dentro da d'água que pode ser praticado com ou sem o
-                        auxílio de um aparelho de respiração. Ele pode ser realizado de forma autônoma, livre 
-                        (controlando a respiração) ou dependente. 
-                        </Texto>
-                        <Texto>
-                        Infelizmente o homem não tem a capacidade de sobreviver em ambiente aquático. Para superar 
-                        essa limitação, foram desenvolvidas técnicas denominadas de mergulho, que tal entrar na Grand Blue?
-                        </Texto>
-                    </Paragrafos>
-                
-                    <Users />
-                    <Footer />
-        </Container>
-</>
-    )
+          <Seção>
+            <Titulos>
+              <Titulo>
+                “Que desgraça é para o
+                  <br />  
+                homem envelhecer sem
+                  <br /> 
+                nunca ver a beleza e a força
+                  <br /> 
+                do que o seu corpo é capaz”
+              </Titulo>
+              <span>Sócrates</span >
+            </Titulos> 
+          </Seção>
+
+          <Cartão>
+            <TextoCartão>
+              Seja bem-vindo à plataforma de artigos voltado à saúde.
+
+              <ImagemCartão 
+                src={loguinha}>
+              </ImagemCartão>
+            </TextoCartão>
+
+            <TituloCartão>
+              O QUE É A HEALTHFUL ?
+            </TituloCartão>
+
+            <TextoCartão>
+              A HEALTHFUL É UMA PLATAFORMA DE ARTIGOS VOLTADO AO SETOR DA SAÚDE, COM O FOCO EM MELHORIA DE VIDA. ACESSE ABAIXO PARA LER NOSSOS ARTIGOS OU CRIE UM ARTIGO.
+            </TextoCartão>
+
+            <TituloCartão>
+              CRIE OU LEIE ARTIGOS
+            </TituloCartão>
+
+          </Cartão>
+                    
+        <Footer />
+      </Container>
+    </>
+  )
 }
--- Healthful/src/Pages/Home/style.jsx
+++ Healthful/src/Pages/Home/style.jsx
@@ -1,89 +1,82 @@
-import styled from "styled-components"
-import banner from "../../Assets/banner_home.svg"
+import styled from "styled-components";
+import banner from "../../Assets/banner_home.svg";
 
 const Container = styled.div`
-    display: block;
-    overflow: auto;
-    position: fixed;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    background-color: #414141;
-    height: 100vh;
-    width: 100vw;
-
+  display: block;
+  overflow: auto;
+  position: fixed;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #414141;
+  height: 100vh;
+  width: 100vw;
 `;
 
-const Secao = styled.div`
-    display: flex;
-    justify-content:center;
-    align-items:center;
-    align-content:center;
-    width: 100%;
-    min-height: 88%;
-    max-height: fit-content;
-    background-image: url(${banner});
-    background-size: cover;
-    box-shadow: 0px 4px 4px 2px rgba(0,0,0,0.1);
-    word-wrap: break-word
-
-
-    
-
+const Seção = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+  width: 100%;
+  min-height: 88%;
+  max-height: fit-content;
+  background-image: url(${banner});
+  background-size: cover;
+  box-shadow: 0px 4px 4px 2px rgba(0,0,0,0.1);
+  word-wrap: break-word;
 `;
 
 const Titulos = styled.div`
-    display:flex;
-    flex-direction:column;
-    height: 20%;
-    text-align: center;
-    margin-right: 3%;
-    margin-left: auto;
+  display: flex;
+  flex-direction: column;
+  height: 20%;
+  text-align: center;
+  margin-right: 3%;
+  margin-left: auto;
 
-    & span {
-        margin: 3%;
-        font-size: 23px;
-        font-family: 'Alata', sans-serif;
-        color: white;
-    }
-    
-
+  & span {
+    margin: 3%;
+    font-size: 23px;
+    color: white;
+  }
 `;
 
 
 const Titulo = styled.p`
-    font-family: 'Alegreya SC', serif;
-    font-size: 45px;        
-    color: white;
-    justify-content: center;
-    position: relative;
-    
-
+  font-size: 45px;        
+  color: white;
+  justify-content: center;
+  position: relative;
 `;
 
-
-
-
-const Paragrafos = styled.div`
-    display: flex;
-    justify-content:center;
-    text-align: right;
-    align-items: right;
-    width: 100%;
-    
-
+const Cartão = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  align-items: center;
+  background: white;
+  flex-direction: column;
 `;
 
-
-const Texto = styled.p`
-    font-family: 'Abyssinica', Georgia, 'Times New Roman', Times, serif;
-    font-size: 1rem;
-    color: white;
-    font-weight: lighter;
-    text-align: justify;
-    width: 50%;
-    margin: 3rem;
-    font-size: 1.1rem;
+const TituloCartão = styled.h1`
+width: 50%;
+font-size: 3rem;
+font-weight: lighter;
 `;
 
-export { Container, Secao, Titulo, Titulos, Paragrafos, Texto} 
+const TextoCartão = styled.p`
+  width: 50%;
+  margin: 3rem;
+  color: black;
+  text-align: center;
+  font-size: 1.2rem;
+  font-weight: lighter;
+`;
+
+const ImagemCartão = styled.img`
+  height: 8rem;
+`;
+
+export { Container, Seção, Titulo, Titulos, Cartão, TextoCartão, TituloCartão, ImagemCartão} 
